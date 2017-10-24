3d0f8ebd2a82e642a648c8a9258813c79f55e225
unknown <casca_de_fier@yahoo.com>
2017-10-24 08:50:23 +0300

    gulp add1

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,13 @@
 let gulp = require('gulp');
+let scss = require('gulp-sass');
+let uglify = require('gulp-uglify');
+let css = require('gulp-clean-css');
+let fs = require('fs');
+let concat = require('gulp-concat');
+let plumber = require('gulp-plumber');
+let plugins = require('gulp-load-plugins');
+let run = require('run-sequence');
+let img = require('gulp-imagemin');
 
 gulp.task('connect', () => {
     plugins().connect.server({
